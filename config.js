1abe19a40724750d3e25d8d1085bbcc8d86dcd02
unknown <fedorchuk.kp93@gmail.com>
2021-05-27 23:19:49 +0300

    produc ver commited

--- config.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-
-module.exports = {salt: process.env.VUE_APP_SALT};
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/" + process.env.CI_PROJECT_NAME + "/"
-      : "/",
-};
